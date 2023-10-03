b26643cf58180d643241f77a8c64266d6930ecd9
andersondfm <anderson.migloranza@gmail.com>
2023-10-03 11:44:01 -0300

    Ajustes de Token

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProdutosComponent } from './produtos/produtos.component';
-import {ProdutosEditComponent } from './produtos/produtos-edit.component';
+import { ProdutosEditComponent } from './produtos/produtos-edit.component';
 import { LoginComponent } from './auth/login.component';
+import { AuthGuard } from './auth/auth.guard';
+
 
 const routes: Routes = [
-  { path: 'produtos', component: ProdutosComponent},
-  { path: 'produto', component: ProdutosEditComponent},
-  { path: 'produto/:id', component: ProdutosEditComponent},
+  { path: 'produtos', component: ProdutosComponent, canActivate: [AuthGuard] },
+  { path: 'produto', component: ProdutosEditComponent, canActivate: [AuthGuard] },
+  { path: 'produto/:id', component: ProdutosEditComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Andersondfm';
+  private destroySubject = new Subject();
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService,
+    private router: Router) {
+    this.authService.authStatus
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe(result => {
+        this.isLoggedIn = result;
+      })
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(["/login"]);
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
+
+  ngOnDestroy() {
+    this.destroySubject.next(true);
+    this.destroySubject.complete();
+  }
 }
